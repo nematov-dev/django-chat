3229b05dc291643ad5f2f41bd813f67faa8746a0
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-04-09 16:13:34 +0500

    Update asgi

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,4 +1,9 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+
+django.setup()
+
 import django
 from django.core.asgi import get_asgi_application
 
@@ -6,10 +11,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import room.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
-
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
